8511a7ef0a6d22b838b553a0dd5fe3d68c4490c5
Kandy-Kane <frankmyster@gmail.com>
2022-01-30 16:56:51 -0500

    Got Sign in working correctly. Added find user method to database class. May still need some tweaking

--- lib/user_Firebase.dart
+++ lib/user_Firebase.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 // Import the firebase_core and cloud_firestore plugin
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 
 class Database {
   final CollectionReference users =
@@ -21,7 +22,31 @@ class Database {
         .then((value) => print("User Added"))
         .catchError((error) => print("Failed to add user: $error"));
   }
+
+  Future<bool> findUser(String username, String password) async {
+    bool foundEmail = false;
+    bool matched = false;
+    //query the whole collection search by email
+    var user = await users.get().then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        //print(doc["email"]);
+        if (doc['email'] == username) {
+          print("USER FOUND!!!");
+          print(doc.data());
+          foundEmail = true;
+          if (doc['password'] == password) {
+            print("PASSWORD MATCH");
+            matched = true;
+          }
+        }
+      });
+    });
+    return matched;
+  }
 }
+     
+
+
 
 // class AddUser extends StatelessWidget {
 //   final String fullName;
